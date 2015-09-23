67a4f5a5a852a0e81d789d152e6c914eb9048875
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-09-23 17:31:04 -0500

    Update seed file, delete comments from routes.rb, add association to user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+50.times do
+  User.create ({
+    name: Faker::Name.name,
+  })
+end
+
+100.times do
+  doge_start = %w(how such very many)
+  doge_end = %w(wonder tweet title blog marvels sparkle tingle yolo! mystery ROFL OMG totes! post comment lorem ipsum)
+  doge_exclaim = ["???", "!", "! <3", " <3"]
+  doge = 3.times.map { (doge_start.sample + " " + doge_end.sample + doge_exclaim.sample + "\n").capitalize }.join("")
+  Tweet.create ({
+    message: doge,
+    user_id: Faker::Number.between(1, 50)
+  })
+end
